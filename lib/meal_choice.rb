cc33b41dc9c2d34b2ccbc226ebca851240d4f32f
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-19 19:58:04 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,5 @@
 def meal_choice(side_1,side_2,protein = "tofu")
   puts "What a nutritious meal!"
-  return "A plate of #{protein} with #{side_1} and #{side_2}."
+  puts "A plate of #{protein} with #{side_1} and #{side_2}."
+  "A plate of #{protein} with #{side_1} and #{side_2}."
 end
